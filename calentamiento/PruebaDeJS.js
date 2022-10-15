1f2ba761e8b2e53ad5e9cf37d1bec66ccca2fd3a
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-10-15 18:02:18 -0500

    prueba js

---
+++ calentamiento/PruebaDeJS.js
@@ -0,0 +1,17 @@
+//Determina el nombre y tipo de dato para almacenar en variables la siguiente información:
+
+//Strings
+var nombre = "Miguel"; 
+var apellido 
+var nombreDeUsuarioEnPlatzi
+//numero
+var edad
+
+var correoElectrónico
+var mayorEdad
+var dineroAhorrado
+var deudas
+
+function dineroReal (dineroAhorrado,deudas) {
+    dineroReal = d
+}
