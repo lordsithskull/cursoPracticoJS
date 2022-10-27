7ef60a09a5ea12d7eb9ef4d2c5b7b93f0d23404d
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2022-10-26 19:24:01 -0500

    se genera código para triangulos

--- figuras.js
+++ figuras.js
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+//Cuadrado
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden " + ladoCuadrado + " cm");
+
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado ;
+console.log("El área  del cuadrado es " + areaCuadrado + " cm^2");
+
+
+//triangulo
+const ladoMayorTriangulo = 5;
+const ladoMenorTriangulo = 5;
+const baseTriangulo = 5;
+console.log("Los lados del triangulo miden " + ladoMayorTriangulo + " cm " + lado2Triangulo + " cm " + baseTriangulo + " cmMenor");
+
+
+const perimetroTriangulo = ladoMayorTriangulo + lado2Triangulo + baseTriangulo;
+console.log("El perimetro del cMenoradrado es " + perimetroTriangulo + " cm");
+
+const areaTRiangulo = (baseTriangulo * ladoMayorTriangulo) / 2;
+console.log("El área  del cuadrado es " + aMenoreaTRiangulo + " cm^2");
