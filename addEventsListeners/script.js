5708e37c1742853409d7b69f861eaef00fc73b74
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2023-02-14 01:40:04 -0600

    "se termina clase listener"

--- addEventsListeners/script.js
+++ addEventsListeners/script.js
@@ -1,13 +1,26 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
-const pResult = document.querySelector('#result')
+const pResult = document.querySelector('#result');
 
-btn.addEventListener('click', btnOnClick);
+/*Solucion usando preventDefault
+form.addEventListener('submit', sumarInpunts);
 
 
-function btnOnClick() {
+function sumarInpunts(event) {
     const sumaInputs = input1.value + input2.value;
+    event.preventDefault();
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+*/
+
+//cambiando el tipo de button a "button"
+btn.addEventListener('click', sumarInpunts);
+
+function sumarInpunts() {
+    const sumaInputs = input1.value + input2.value;
+    
     pResult.innerText = "Resultado: " + sumaInputs;
 }
